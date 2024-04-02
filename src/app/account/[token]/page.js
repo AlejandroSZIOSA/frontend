24748b738280f9a78567973c9f62a21731582ec6
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-04-02 11:41:15 +0200

    some changes

--- src/app/account/[token]/page.js
+++ src/app/account/[token]/page.js
@@ -1,17 +1,45 @@
+"use client";
 import React from "react";
+import { useEffect, useState } from "react";
 
 export default function test({ params }) {
+  const token = params.token;
+  const [userAccountData, setUserAccountData] = useState({});
+
+  useEffect(() => {
+    getUserSaldo(token);
+    /* getSaldo(userId); */
+  }, []);
+
+  async function getUserSaldo(token) {
+    fetch("http://localhost:4000/me/accounts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token: token }),
+    })
+      .then((response) => response.json())
+      .then((data) => setUserAccountData(data))
+      .catch((error) => {
+        console.error("Error:", error);
+        return "error"; //return error
+      });
+  }
+
+  function handleSaveNewSaldo() {}
+
   return (
     <div className="flex flex-col m-3 gap-4">
-      <div>Token: {params.token}</div>
+      <div>Current Token: {token}</div>
+      <div>Current user Id: {userAccountData.userId}</div>
       <div>
-        <h2> Your Saldo: $</h2>
+        <h2>Current Saldo: $ {userAccountData.amount}</h2>
       </div>
-      <div>
+      <div className="flex flex-col gap-3 items-center">
         <h2>
           In money:
           <input type="number" />
         </h2>
+        <button className="w-36 h-10 bg-slate-400">Save</button>
       </div>
       <div></div>
     </div>
