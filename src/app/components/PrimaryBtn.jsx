fff7ea3093d4efc4f92048ded301a757aaae78e8
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-04-20 09:43:33 +0200

    Fix problem med primary Btn color

--- src/app/components/PrimaryBtn.jsx
+++ src/app/components/PrimaryBtn.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function PrimaryBtn({
   children,
   onClickFn,
-  bgColor = "yellow-700",
+  bgColor = "yellow-700", //TODO:This doesn't work as passing prop?
 }) {
   return (
     <button
-      className={`flex w-36 h-14 bg-${bgColor} text-[#FFF2E1] items-center justify-center rounded-md`}
+      className="flex w-36 h-14 bg-yellow-700 text-[#FFF2E1] items-center justify-center rounded-md"
       onClick={onClickFn}
     >
       {children}
