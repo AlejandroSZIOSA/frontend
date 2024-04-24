d3497fb16574aa7c0af7dfece818f6b853ac2ed0
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-04-24 16:12:26 +0200

    some test

---
+++ src/app/__test__/math.test.js
@@ -0,0 +1,8 @@
+import { expect, it } from "@testing-library/react";
+
+import { add } from "./math.js";
+
+it("should summarize all number values in an array", () => {
+  const result = add([1, 2, 3]);
+  expect(result).toBe(6);
+});
--- src/app/loginUser/page.js
+++ src/app/loginUser/page.js
@@ -38,7 +38,7 @@ export default function loginUser() {
 
   function handleLogin() {
     //NOTE: Test purposes
-    //setIsAuth(true);
+    /* setIsAuth(true); */
 
     const enteredUsername = usernameInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
