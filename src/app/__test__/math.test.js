f5d9746f31c3c56849344cd114fdd39ab301dab3
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-04-25 14:01:32 +0200

    Test fixes

--- src/app/__test__/math.test.js
+++ src/app/__test__/math.test.js
@@ -1,8 +1,6 @@
-import { expect, it } from "@testing-library/react";
-
 import { add } from "./math.js";
 
-it("Should summarize all number values in an array", () => {
+test("Should summarize all number values in an array", () => {
   const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  expect(result).toBe(8);
 });
--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-  //API_BASE_URL: "http://localhost:4000",
-  API_BASE_URL: "http://16.170.15.0:4000",
+  API_BASE_URL: "http://localhost:4000",
+  //API_BASE_URL: "http://16.170.15.0:4000",
 };
 export default CONSTANTS;
